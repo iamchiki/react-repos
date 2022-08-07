153b81a6253c19f4ec5860d41ef8d9230b57ce3a
iamchiki <nikhil.soni599@gmail.com>
2022-08-08 01:21:59 +0530

    refactor: change auth token

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 // Constants
 // github authentication token to access api
-export const AUTH_TOKEN = "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";
+// export const AUTH_TOKEN = "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";
+export const AUTH_TOKEN = process.env.REACT_APP_AUTH_TOKEN;
 export const API_URL = "https://api.github.com/graphql";
