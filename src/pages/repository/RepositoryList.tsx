5048732e333a9b74c1d7ba4efca4e5ed14f80b7e
iamchiki <nikhil.soni599@gmail.com>
2022-08-15 15:40:54 +0530

    refactor: implement apollo client

--- src/pages/repository/RepositoryList.tsx
+++ src/pages/repository/RepositoryList.tsx
@@ -1,27 +1,49 @@
 import { Box } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import SearchInput from "../../components/SearchInput/SearchInput";
 import TableComponent from "../../components/table/TableComponent";
-import { getRepositories } from "../../services/api";
 import { RepoTyp } from "../../utils/types";
 import { BOX_STYLE, CONTAINER_STYLE } from "./styles";
+import { gql, useQuery } from "@apollo/client";
 
-const TOPIC = "react";
+const TOPIC_QUERY = "topic:react";
 const COUNT = 50;
+const GET_REPOS = gql`
+  query Repos($query: String!, $count: Int) {
+    search(type: REPOSITORY, query: $query, first: $count) {
+      repos: edges {
+        repo: node {
+          ... on Repository {
+            name
+            forkCount
+            stargazerCount
+            url
+          }
+        }
+      }
+    }
+  }
+`;
 
 const RepositoryList = () => {
   const [repoList, setRepoList] = useState([]);
-  const [masterList, setMasterList] = useState([]);
+  const { data, loading } = useQuery(GET_REPOS, {
+    variables: { query: TOPIC_QUERY, count: COUNT },
+  });
 
-  useEffect(() => {
-    // to get repolist
-    const fetchRepos = async () => {
-      const response = await getRepositories(TOPIC, COUNT);
-      setRepoList(response);
-      setMasterList(response);
-    };
-    fetchRepos();
-  }, []);
+  const masterList = data?.search?.repos;
+
+  let list: {
+    repo: RepoTyp;
+  }[];
+
+  if (loading) {
+    list = [];
+  } else if (repoList.length !== 0) {
+    list = repoList;
+  } else {
+    list = data.search.repos;
+  }
 
   // filter repos based on input keyword
   const filterRepos = (val: string) => {
@@ -34,10 +56,10 @@ const RepositoryList = () => {
   };
 
   return (
-    <Box sx={BOX_STYLE}>
+    <Box sx={BOX_STYLE} data-testid="repositoryList">
       <SearchInput onChange={filterRepos} />
       <div style={CONTAINER_STYLE}>
-        <TableComponent repos={repoList} />
+        <TableComponent repos={list} />
       </div>
     </Box>
   );
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,42 +1,20 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { API_URL, AUTH_TOKEN } from "../utils/constants";
+import { setContext } from "@apollo/client/link/context";
 
-let myHeaders = new Headers();
-myHeaders.append("Authorization", `Bearer ${AUTH_TOKEN}`);
-myHeaders.append("Content-Type", "application/json");
+const httpLink = createHttpLink({
+  uri: API_URL,
+});
 
-// fetch all repos github api
-export const getRepositories = async (topic: string, totalCount: number) => {
-  let repoList = [];
-
-  const graphql = JSON.stringify({
-    query: `{
-      search(type: REPOSITORY, query: "topic:${topic}", last: ${totalCount}) {
-        repos: edges {
-          repo: node {
-            ... on Repository {
-              name
-              forkCount
-              stargazerCount
-              url
-            }
-          }
-        }
-      }
-    }`,
-    variables: {},
-  });
-
-  const requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: graphql,
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${AUTH_TOKEN}`,
+    },
   };
-  try {
-    const url = API_URL; //github api url
-    const response = await fetch(url, requestOptions as RequestInit);
-    const result = await response.json();
-    repoList = result.data.search.repos;
-  } catch (error) {}
-
-  return repoList;
-};
+});
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
