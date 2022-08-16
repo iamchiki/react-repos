fe229e745f7730f8ce6b9ce929d8d340de3bc502
iamchiki <nikhil.soni599@gmail.com>
2022-08-16 15:03:30 +0530

    fix: resolve test case issue

--- src/pages/repository/RepositoryList.test.tsx
+++ src/pages/repository/RepositoryList.test.tsx
@@ -1,9 +1,15 @@
+import { ApolloProvider } from "@apollo/client";
 import { render, screen } from "@testing-library/react";
+import { client } from "../../services/api";
 import RepositoryList from "./RepositoryList";
 
 describe("test RepositoryList", () => {
   it("should render RepositoryList component", () => {
-    render(<RepositoryList></RepositoryList>);
+    render(
+      <ApolloProvider client={client}>
+        <RepositoryList></RepositoryList>
+      </ApolloProvider>
+    );
     const repositoryList = screen.getByTestId("repositoryList");
     expect(repositoryList).toBeTruthy();
   });
